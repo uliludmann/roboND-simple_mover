4d8c9cf0ea746fa9c57992f48bc72bc5292ffdbc
Unknown <ulrichludmann@gmail.com>
2017-09-14 07:18:57 -0700

    my first service implementation

--- catkin_ws/build/catkin_generated/setup_cached.sh
+++ catkin_ws/build/catkin_generated/setup_cached.sh
@@ -7,9 +7,4 @@
 # new environment variables
 
 # modified environment variables
-export CMAKE_PREFIX_PATH="/home/robond/Documents/RoboND-simple_mover/catkin_ws/devel:/opt/ros/kinetic"
-export LD_LIBRARY_PATH="/home/robond/Documents/RoboND-simple_mover/catkin_ws/devel/lib:/opt/ros/kinetic/lib:/opt/ros/kinetic/lib/x86_64-linux-gnu"
-export PKG_CONFIG_PATH="/opt/ros/kinetic/lib/pkgconfig:/opt/ros/kinetic/lib/x86_64-linux-gnu/pkgconfig"
 export PWD="/home/robond/Documents/RoboND-simple_mover/catkin_ws/build"
-export ROSLISP_PACKAGE_DIRECTORIES="/home/robond/Documents/RoboND-simple_mover/catkin_ws/devel/share/common-lisp"
-export ROS_PACKAGE_PATH="/home/robond/Documents/RoboND-simple_mover/catkin_ws/src:/opt/ros/kinetic/share"
